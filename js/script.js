b0be17eebfdd1a731c374df517ea6765c5a87d5a
Laurabinky <91797246+Laurabinky@users.noreply.github.com>
2023-03-14 16:16:15 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+console.log("hi");
 
+// Navigatie
+// menu openen
+var openButton = document.querySelector("header > button");
+
+openButton.addEventListener("click", menuOpenen);
+
+function menuOpenen() {
+	var deNav = document.querySelector("header > nav");
+	
+	deNav.classList.add("openen");
+}
+
+
+// menu sluiten 
+var sluitButton = document.querySelector("header nav button");
+
+sluitButton.addEventListener("click", menuSluiten);
+
+function menuSluiten() {
+	var deNav = document.querySelector("header > nav");
+	
+	deNav.classList.remove("openen");
+}
